ac97ab0acb9f9ac5dc0880d323fb3d9bbc546286
Pilchh <alex@thepilchers.com>
2026-01-09 23:52:52 +0000

    Implemented test for vector from_vec

--- src/vector/vector.rs
+++ src/vector/vector.rs
@@ -6,12 +6,35 @@ pub struct Vector {
 }
 
 impl Vector {
+    /// Creates a new Vector with provided data
+    ///
+    /// # Arguments
+    /// * `data` - the data to be stored in the vector
+    ///
+    /// # Returns
+    /// A new `Vector` with defined data.
+    ///
+    /// # Example
+    /// ```
+    /// use linears::vector::Vector;
+    ///
+    /// let data = vec![1.0, 2.0, 3.0];
+    /// let vector = Vector::from_vec(data);
+    /// ```
     pub fn from_vec(data: Vec<f64>) -> Vector {
         Vector {
             size: data.len(),
             data,
         }
     }
+
+    //     pub fn get(&self, index: usize) -> Result<f64, VectorOperationError>;
+    //     pub fn set(&mut self, index: usize, value: f64) -> Result<(), VectorOperationError>;
+    //
+    //     pub fn add(&self, other: &Vector) -> Result<Vector, VectorOperationError>;
+    //     pub fn sub(&self, other: &Vector) -> Result<Vector, VectorOperationError>;
+    //     pub fn scalar_mul(&self, scalar: f64) -> Vector;
+    //     pub fn dot(&self, other: &Vector) -> Result<f64, VectorOperationError>;
 }
 
 impl Index<usize> for Vector {
@@ -28,12 +51,19 @@ impl IndexMut<usize> for Vector {
     }
 }
 
-// impl Vector {
-//     pub fn get(&self, index: usize) -> Result<f64, VectorOperationError>;
-//     pub fn set(&mut self, index: usize, value: f64) -> Result<(), VectorOperationError>;
-//
-//     pub fn add(&self, other: &Vector) -> Result<Vector, VectorOperationError>;
-//     pub fn sub(&self, other: &Vector) -> Result<Vector, VectorOperationError>;
-//     pub fn scalar_mul(&self, scalar: f64) -> Vector;
-//     pub fn dot(&self, other: &Vector) -> Result<f64, VectorOperationError>;
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vector_from_vec() {
+        // Arrange
+        let data = vec![1.0, 2.0, 3.0];
+
+        // Act
+        let v = Vector::from_vec(data);
+
+        // Assert
+        assert_eq!(v.data, vec![1.0, 2.0, 3.0]);
+    }
+}
