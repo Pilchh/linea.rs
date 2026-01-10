db3f0a406f82f74cc5301e8057a8dec1e646ed46
Pilchh <alex@thepilchers.com>
2026-01-10 15:05:00 +0000

    Implemented arithmetic operations for vector type

--- src/vector/errors.rs
+++ src/vector/errors.rs
@@ -4,4 +4,6 @@ use thiserror::Error;
 pub enum VectorOperationError {
     #[error("index is out of bounds (tried to get index {index} on size {size})")]
     OutOfBounds { index: usize, size: usize },
+    #[error("the dimensions of the two vectors are not the same (got {a_size} and {b_size})")]
+    InvalidDimensions { a_size: usize, b_size: usize },
 }
--- src/vector/vector.rs
+++ src/vector/vector.rs
@@ -69,11 +69,11 @@ impl Vector {
     /// ```
     /// use linears::vector::Vector;
     ///
-    /// let vector = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+    /// let mut vector = Vector::from_vec(vec![1.0, 2.0, 3.0]);
     ///
-    /// let value = vector.set(1, 20.0).unwrap();
+    /// vector.set(1, 20.0).unwrap();
     ///
-    /// assert_eq!(value, 20.0);
+    /// assert_eq!(vector.get(1).unwrap(), 20.0);
     /// ```
     pub fn set(&mut self, index: usize, value: f64) -> Result<(), VectorOperationError> {
         if index >= self.size {
@@ -86,11 +86,147 @@ impl Vector {
         self.data[index] = value;
         Ok(())
     }
-    //
-    //     pub fn add(&self, other: &Vector) -> Result<Vector, VectorOperationError>;
-    //     pub fn sub(&self, other: &Vector) -> Result<Vector, VectorOperationError>;
-    //     pub fn scalar_mul(&self, scalar: f64) -> Vector;
-    //     pub fn dot(&self, other: &Vector) -> Result<f64, VectorOperationError>;
+
+    /// Adds two vectors together
+    ///
+    /// # Arguments
+    /// * `other` - the vector to add
+    ///
+    /// # Returns
+    /// A new `Vector` with the computed value.
+    ///
+    /// # Example
+    /// ```
+    /// use linears::vector::Vector;
+    /// let a = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+    /// let b = Vector::from_vec(vec![5.0, 6.0, 7.0]);
+    ///
+    /// let result = a.add(&b).unwrap();
+    ///  
+    /// assert_eq!(result.data, vec![6.0, 8.0, 10.0]);
+    /// ```
+    pub fn add(&self, other: &Vector) -> Result<Vector, VectorOperationError> {
+        if self.size != other.size {
+            return Err(VectorOperationError::InvalidDimensions {
+                a_size: self.size,
+                b_size: other.size,
+            });
+        }
+
+        let mut result_vector = Vector {
+            size: self.size,
+            data: Vec::with_capacity(self.size),
+        };
+
+        for i in 0..self.size {
+            result_vector.data.push(self.data[i] + other.data[i]);
+        }
+
+        Ok(result_vector)
+    }
+
+    /// Substracts one vector from another
+    ///
+    /// # Arguments
+    /// * `other` - the vector to subtract
+    ///
+    /// # Returns
+    /// A new `Vector` with the computed value.
+    ///
+    /// # Example
+    /// ```
+    /// use linears::vector::Vector;
+    /// let a = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+    /// let b = Vector::from_vec(vec![5.0, 6.0, 7.0]);
+    ///
+    /// let result = a.sub(&b).unwrap();
+    ///  
+    /// assert_eq!(result.data, vec![-4.0, -4.0, -4.0]);
+    /// ```
+    pub fn sub(&self, other: &Vector) -> Result<Vector, VectorOperationError> {
+        if self.size != other.size {
+            return Err(VectorOperationError::InvalidDimensions {
+                a_size: self.size,
+                b_size: other.size,
+            });
+        }
+
+        let mut result_vector = Vector {
+            size: self.size,
+            data: Vec::with_capacity(self.size),
+        };
+
+        for i in 0..self.size {
+            result_vector.data.push(self.data[i] - other.data[i]);
+        }
+
+        Ok(result_vector)
+    }
+
+    /// Multiplies a vector with a scalar
+    ///
+    /// # Arguments
+    /// * `scalar` - the scalar to multiply by
+    ///
+    /// # Returns
+    /// A new `Vector` with the computed value.
+    ///
+    /// # Example
+    /// ```
+    /// use linears::vector::Vector;
+    /// let v = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+    ///
+    /// let result = v.scalar_mul(10.0);
+    ///  
+    /// assert_eq!(result.data, vec![10.0, 20.0, 30.0]);
+    /// ```
+    pub fn scalar_mul(&self, scalar: f64) -> Vector {
+        let mut result_vector = Vector {
+            size: self.size,
+            data: Vec::with_capacity(self.size),
+        };
+
+        for i in 0..self.size {
+            result_vector.data.push(self.data[i] * scalar);
+        }
+
+        result_vector
+    }
+
+    /// Calculates to dot product of two vectors
+    ///
+    /// # Arguments
+    /// * `other` - the second vector
+    ///
+    /// # Returns
+    /// A new `Vector` with the computed value.
+    ///
+    /// # Example
+    /// ```
+    /// use linears::vector::Vector;
+    /// let a = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+    /// let b = Vector::from_vec(vec![4.0, 5.0, 6.0]);
+    ///
+    /// let result = a.dot(&b).unwrap();
+    ///  
+    /// assert_eq!(result, 32.0);
+    /// ```
+    pub fn dot(&self, other: &Vector) -> Result<f64, VectorOperationError> {
+        if self.size != other.size {
+            return Err(VectorOperationError::InvalidDimensions {
+                a_size: self.size,
+                b_size: other.size,
+            });
+        }
+
+        let mut result = 0.0;
+
+        for i in 0..self.size {
+            result += self.data[i] * other.data[i];
+        }
+
+        Ok(result)
+    }
 }
 
 impl Index<usize> for Vector {
@@ -104,6 +240,30 @@ impl Index<usize> for Vector {
 impl IndexMut<usize> for Vector {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.data[index]
+    }
+}
+
+impl std::ops::Add for &Vector {
+    type Output = Vector;
+
+    fn add(self, other: Self) -> Vector {
+        self.add(other).unwrap()
+    }
+}
+
+impl std::ops::Sub for &Vector {
+    type Output = Vector;
+
+    fn sub(self, other: Self) -> Vector {
+        self.sub(other).unwrap()
+    }
+}
+
+impl std::ops::Mul<f64> for &Vector {
+    type Output = Vector;
+
+    fn mul(self, scalar: f64) -> Vector {
+        self.scalar_mul(scalar)
     }
 }
 
@@ -145,5 +305,56 @@ mod tests {
 
         // Assert
         assert_eq!(v.data[1], 20.0);
+    }
+
+    #[test]
+    fn test_vector_add() {
+        // Arrange
+        let a = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+        let b = Vector::from_vec(vec![4.0, 5.0, 6.0]);
+
+        // Act
+        let result = &a + &b;
+
+        // Assert
+        assert_eq!(result.data, vec![5.0, 7.0, 9.0]);
+    }
+
+    #[test]
+    fn test_vector_sub() {
+        // Arrange
+        let a = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+        let b = Vector::from_vec(vec![4.0, 5.0, 6.0]);
+
+        // Act
+        let result = &a - &b;
+
+        // Assert
+        assert_eq!(result.data, vec![-3.0, -3.0, -3.0]);
+    }
+
+    #[test]
+    fn test_vector_dot() {
+        // Arrange
+        let a = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+        let b = Vector::from_vec(vec![4.0, 5.0, 6.0]);
+
+        // Act
+        let result = a.dot(&b).unwrap();
+
+        // Assert
+        assert_eq!(result, 32.0);
+    }
+
+    #[test]
+    fn test_vector_scalar_mul() {
+        // Arrange
+        let v = Vector::from_vec(vec![1.0, 2.0, 3.0]);
+
+        // Act
+        let result = &v * 10.0;
+
+        // Assert
+        assert_eq!(result.data, vec![10.0, 20.0, 30.0]);
     }
 }
